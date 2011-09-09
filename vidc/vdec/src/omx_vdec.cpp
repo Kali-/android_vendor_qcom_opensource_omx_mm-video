5f97e73bd6212ce4f61cbb21b2675cfe0b628399
Deepika Pepakayala <deepikap@codeaurora.org>
2011-09-09 15:54:15 -0700

    mm-video: vdec: Add null check for native buffer.
    Add null check for the native buffer pointer forwarded
    to the omx component by the client.
    
    Change-Id: I8f6116814f2513ede7b4720470c3ed4986c2828d
    CRs-Fixed: 307873

--- vidc/vdec/src/omx_vdec.cpp
+++ vidc/vdec/src/omx_vdec.cpp
@@ -2702,10 +2702,15 @@ OMX_ERRORTYPE omx_vdec::use_android_native_buffer(OMX_IN OMX_HANDLETYPE hComp, O
     DEBUG_PRINT_LOW("Inside use_android_native_buffer");
     OMX_ERRORTYPE eRet = OMX_ErrorNone;
     UseAndroidNativeBufferParams *params = (UseAndroidNativeBufferParams *)data;
+
+    if((params == NULL) ||
+       (params->nativeBuffer == NULL) ||
+       (params->nativeBuffer->handle == NULL) ||
+       !m_enable_android_native_buffers)
+        return OMX_ErrorBadParameter;
+
     sp<android_native_buffer_t> nBuf = params->nativeBuffer;
     private_handle_t *handle = (private_handle_t *)nBuf->handle;
-    if(!params || !m_enable_android_native_buffers)
-        return OMX_ErrorBadParameter;
     if(OMX_CORE_OUTPUT_PORT_INDEX == params->nPortIndex) {  //android native buffers can be used only on Output port
         OMX_U8 *buffer = (OMX_U8*)mmap(0, handle->size,
                 PROT_READ|PROT_WRITE, MAP_SHARED, handle->fd, 0);
