10be5b5d69588062aa8501f479c9cf9c2a3fc2b3
Giulio Cervera <giulio.cervera@gmail.com>
2012-02-20 22:22:54 +0100

    mm-video: update mk for cm9
    Change-Id: I73a1bd021b53ecc4dda7f1490a0e145cdbdcc91a

--- Android.mk
+++ Android.mk
@@ -1,25 +1,29 @@
 OMX_VIDEO_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-ifeq ($(call is-board-platform-in-list,$(QSD8K_BOARD_PLATFORMS)),true)
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
     include $(OMX_VIDEO_PATH)/qdsp6/vdec/Android.mk
     include $(OMX_VIDEO_PATH)/qdsp6/venc/Android.mk
 endif
 
-ifeq ($(call is-chipset-in-board-platform,msm7630),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
     include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
     include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
     include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
 endif
 
-ifeq ($(call is-board-platform,msm8660),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
     include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
     include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
     include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
 endif
 
-ifeq ($(call is-board-platform,msm8960),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
     include $(OMX_VIDEO_PATH)/vidc/vdec/Android.mk
     include $(OMX_VIDEO_PATH)/vidc/venc/Android.mk
     include $(OMX_VIDEO_PATH)/DivxDrmDecrypt/Android.mk
 endif
+
+endif #BOARD_USES_QCOM_HARDWARE
