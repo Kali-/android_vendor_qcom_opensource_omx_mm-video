131ad593d595e512ba6c3df4b9900c1a14a09952
Deepak Kotur <dkotur@codeaurora.org>
2012-05-15 14:54:02 -0700

    vdec: Fix for free output buffers
    As the ion device fd is closed in the memoryheapION
    the ioctl for ION_IOC_FREE was failing, Moved the
    free_ion_memory() before the memoryheapION destructor
    is called.
    
    (cherry picked from commit 3281f9a194635123e848f2b02d75e7092794ab66)
    
    Change-Id: I13b01eb1baf0bed559532dc63d90ba063dbcb4f7

--- vidc/vdec/src/omx_vdec.cpp
+++ vidc/vdec/src/omx_vdec.cpp
@@ -4367,6 +4367,11 @@ OMX_ERRORTYPE omx_vdec::free_output_buffer(OMX_BUFFERHEADERTYPE *bufferHdr)
                     munmap (drv_ctx.ptr_outputbuffer[index].bufferaddr,
                             drv_ctx.ptr_outputbuffer[index].mmaped_size);
                }
+#ifdef USE_ION
+                free_ion_memory(&drv_ctx.op_buf_ion_info[index]);
+#endif
+                close (drv_ctx.ptr_outputbuffer[index].pmem_fd);
+                drv_ctx.ptr_outputbuffer[index].pmem_fd = -1;
 #ifdef _ANDROID_
                 m_heap_ptr[index].video_heap_ptr = NULL;
                 m_heap_count = m_heap_count - 1;
@@ -4376,11 +4381,6 @@ OMX_ERRORTYPE omx_vdec::free_output_buffer(OMX_BUFFERHEADERTYPE *bufferHdr)
                     m_heap_ptr = NULL;
                 }
 #endif // _ANDROID_
-                close (drv_ctx.ptr_outputbuffer[index].pmem_fd);
-                drv_ctx.ptr_outputbuffer[index].pmem_fd = -1;
-#ifdef USE_ION
-                free_ion_memory(&drv_ctx.op_buf_ion_info[index]);
-#endif
           }
 #ifdef _ANDROID_
        }
